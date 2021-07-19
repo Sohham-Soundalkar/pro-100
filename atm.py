88b80da774737376a400a8673e3cbdc74873ef86
Sohham-Soundalkar <74593541+Sohham-Soundalkar@users.noreply.github.com>
2021-07-19 14:48:04 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -8,8 +8,23 @@ class Atm (object):
 
     def withdrawl(self, amount):
         new_amount = 50000 - amount
-        print('Your have withdrawn amount'+str(amount) + '. Your remaining balance is '+str(new_amount))
+        print('Your have withdrawn amount '+str(amount) + '. Your remaining balance is '+str(new_amount))
 
 def infoReceiver():
     cardNumber = input('Enter your card number: ')
     pinNumber = input('Enter your pin number: ')
+    newUser = Atm(cardNumber, pinNumber)
+    print('Choose what you want to do: ')
+    print('1.Balance Enquiry      2.Cash Withdrawl')
+    activity = int(input('Enter activity number: '))
+    if(activity==1):
+        newUser.check_balance()
+
+    elif(activity==2):
+        amount = int(input('Enter the amount you wish to withdraw: '))
+        newUser.withdrawl(amount)
+
+    else:
+        print('Enter a valid number: ')
+
+infoReceiver()
